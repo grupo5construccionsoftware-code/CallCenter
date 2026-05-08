1f4bd33930f480f08f9a038d89c5c7d22f74a422
Pineda_WC <72749649@continental.edu.pe>
2026-05-08 00:59:23 -0500

    se le agrego un mensaje de respuesta de guardado a la interfaz de adicional2.jsp.jsp a

--- src/main/java/com/example/CallCenter/Empresa/EmpresaRepository.java
+++ src/main/java/com/example/CallCenter/Empresa/EmpresaRepository.java
@@ -10,7 +10,7 @@ public class EmpresaRepository implements EmpresaDAO {
 
 
     private final List<Empresa> listaSimulada = new ArrayList<>();
-    private int contadorId = 4
+    private int contadorId = 2
             ;
 
     @Override
