6f48953e4fa8c724e96a1de491205c30fa3a69c4
Pineda_WC <72749649@continental.edu.pe>
2026-05-08 01:07:10 -0500

    cambiar el codigo

--- src/main/java/com/example/CallCenter/Empresa/EmpresaRepository.java
+++ src/main/java/com/example/CallCenter/Empresa/EmpresaRepository.java
@@ -10,7 +10,7 @@ public class EmpresaRepository implements EmpresaDAO {
 
 
     private final List<Empresa> listaSimulada = new ArrayList<>();
-    private int contadorId = 3
+    private int contadorId = 2
             ;
 
     @Override
