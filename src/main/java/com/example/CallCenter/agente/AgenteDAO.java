ae2710605f06d41a9f7fa8ccae000f573f8b8c89
zerotwo752 <71127773@continental.edu.pe>
2026-05-30 00:55:40 -0500

    Update AgenteDAO.java

--- src/main/java/com/example/CallCenter/agente/AgenteDAO.java
+++ src/main/java/com/example/CallCenter/agente/AgenteDAO.java
@@ -8,6 +8,8 @@ public interface AgenteDAO {
 
     Agente obtenerAgentePorId(int id_agente);
 
+    Agente obtenerPorCredenciales(String usuario, String contrasenia);
+
     boolean existeTelefonoAgente(String telefono_agente, int idAgenteExcluir);
 
     void crearAgente(Agente agente);
