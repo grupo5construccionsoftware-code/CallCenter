88268f7668f34a6402eccc755c50d694e0710639
zerotwo752 <71127773@continental.edu.pe>
2026-05-30 00:58:58 -0500

    Update LlamadaServiceImpl.java

--- src/main/java/com/example/CallCenter/llamada/LlamadaServiceImpl.java
+++ src/main/java/com/example/CallCenter/llamada/LlamadaServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.CallCenter.llamada;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -15,6 +16,12 @@ public class LlamadaServiceImpl implements LlamadaService {
 
     @Override
     public List<Llamada> listarLlamadas() { return llamadaDAO.listarLlamadas(); }
+
+    @Override
+    public List<Llamada> listarLlamadasPorAgente(int idAgente) { return llamadaDAO.listarLlamadasPorAgente(idAgente); }
+
+    @Override
+    public List<Llamada> listarLlamadasPorAgentes(Collection<Integer> idsAgentes) { return llamadaDAO.listarLlamadasPorAgentes(idsAgentes); }
 
     @Override
     public Llamada obtenerLlamadaPorId(int id_llamada) { return llamadaDAO.obtenerLlamadaPorId(id_llamada); }
