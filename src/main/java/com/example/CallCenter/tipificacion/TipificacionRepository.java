a1b0ed2bfa5405411bba5febf7c376985f789e42
zerotwo752 <71127773@continental.edu.pe>
2026-06-05 03:36:58 -0500

    Update TipificacionRepository.java

--- src/main/java/com/example/CallCenter/tipificacion/TipificacionRepository.java
+++ src/main/java/com/example/CallCenter/tipificacion/TipificacionRepository.java
@@ -1,12 +1,18 @@
 package com.example.CallCenter.tipificacion;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
+
 
 @Repository
 public interface TipificacionRepository extends JpaRepository<Tipificacion, Integer> {
 
-    // Tipificaciones activas
-    List<Tipificacion> findByEstado_tipo(String estado_tipo);
+    @Query("SELECT t FROM Tipificacion t WHERE UPPER(t.estado_tipo) = UPPER(:estadoTipo)")
+    List<Tipificacion> findByEstado(@Param("estadoTipo") String estadoTipo);
+
+    @Query("SELECT t FROM Tipificacion t WHERE UPPER(t.motivo_tipo) = UPPER(:motivoTipo)")
+    Optional<Tipificacion> findByMotivoTipoIgnoreCase(@Param("motivoTipo") String motivoTipo);
 }
